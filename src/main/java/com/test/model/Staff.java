7856b97b6f591c9102906f63167e46b2ddcd72c8
hoangtranduyLONG <duylonghindu@gmail.com~>
2022-08-24 22:22:12 +0700

    done

--- src/main/java/com/test/model/Staff.java
+++ src/main/java/com/test/model/Staff.java
@@ -21,7 +21,7 @@ public class Staff  {
     @Min(value = 18,message = "tuoi phai lon hon 18")
     @Max(value = 35,message = "tuoi phai  nho hon 35")
     private int age;
-    @NumberFormat(pattern = "" + "usd")
+    @NumberFormat(pattern = "" + "")
     private double salary;
 
     @ManyToOne
